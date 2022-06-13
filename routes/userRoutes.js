b4930d3c3553cd8d0b2badf1429affbf6ce4d3d8
Radomyr Horban <horban.radomyr@gmail.com>
2022-06-13 12:34:53 +0300

    Try 2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,14 +9,23 @@ const { responseMiddleware } = require("../middlewares/response.middleware");
 const router = Router();
 
 // TODO: Implement route controllers for user
-router.get("/api/users", createUserValid, function (req, res) {
-  res.send("GET /api/users");
-  console.log(req.body);
+router.get("/api/users", function (req, res) {
+  res.send("/api/users");
 });
-
+router.get("/api/users/:id", function (req, res) {
+  res.send("/api/users/:id");
+});
 router.post("/api/users", function (req, res) {
-  console.log(req.body);
-  res.send("POST /api/users/:id");
+  console.log(req.params);
+  res.send("PUT /api/users/:id");
+});
+router.put("/api/users/:id", function (req, res) {
+  console.log(req.params);
+  res.send("PUT /api/users/:id");
+});
+router.delete("/api/users/:id", function (req, res) {
+  console.log(req.params);
+  res.send("DELETE /api/users/:id");
 });
 
 module.exports = router;
